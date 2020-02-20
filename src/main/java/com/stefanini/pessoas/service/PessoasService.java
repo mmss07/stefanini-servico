17beafc7cb55c51a137dbf2e964237fb58629470
mmss07 <marcelomauricio@hotmail.com>
2020-02-20 20:44:16 -0300

    Correção do cadastro

--- src/main/java/com/stefanini/pessoas/service/PessoasService.java
+++ src/main/java/com/stefanini/pessoas/service/PessoasService.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.stefanini.pessoas.model.Pessoa;
 import com.stefanini.pessoas.repository.Pessoas;
+import com.stefanini.pessoas.util.StringUtil;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -27,9 +29,16 @@ public class PessoasService {
 	@Autowired
 	private Pessoas pessoas;
 	
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Pessoa adicionar(@Valid @RequestBody Pessoa pessoa) {
-		return pessoas.save(pessoa);
+	//@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping
+	public ResponseEntity<Pessoa> adicionar(@Valid @RequestBody Pessoa pessoa) {
+		if(StringUtil.isCPF(pessoa.getCpf())) {
+			pessoa = pessoas.save(pessoa);
+			return ResponseEntity.ok(pessoa);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+		
 	}
 	
 	@GetMapping
